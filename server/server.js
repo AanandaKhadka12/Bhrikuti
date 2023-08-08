0b2d9c959e53e42e374a22689cce04e55f87bdb7
Jasmina348 <jasminakunwar348@gmail.com>
2023-08-08 15:30:04 -0400

    contact service added

--- server/server.js
+++ server/server.js
@@ -3,7 +3,9 @@ const cors = require('cors');
 const express = require('express');
 const mongoose = require('mongoose');
 const mongoString ="mongodb://localhost:27017/Bhrikuti";
-const routes = require('./routes/userRoutes');
+const userRouter = require('./routes/userRoutes');
+const contactRouter = require('./routes/contactRoutes');
+
 
 mongoose.connect(mongoString);
 const database = mongoose.connection;
@@ -20,7 +22,9 @@ app.use(cors())
 app.use(express.json());
 
 
-app.use('/api', routes)
+app.use('/api', userRouter)
+app.use('/api', contactRouter)
+
 
 
 app.listen(3001, () => {
